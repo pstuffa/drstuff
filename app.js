78afa60bb649d18e2dad0ac2b7816a039db99e31
pstuffa <p.buffa@gmail.com>
2017-03-27 22:18:35 -0400

    added stuff

---
+++ app.js
@@ -0,0 +1,31 @@
+// Load in the node modules, give them variable names
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000
+
+// ejs is a way to make html from static file to a template.
+// This can be used to change from html to jade (now pug)
+app.engine('html', require('ejs').renderFile); 
+
+// Set the enginge to html
+app.set('view engine', 'html');
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+
+
+app.get('/', function (req, res) {
+  	res.render('index.html');
+});
+
+//heroku assigns app port randomly. DONT WORRY ABOUT IT
+app.set('port', port);
+
+// console log here actually sends to your terminal
+app.listen(port, function () {
+  console.log('Example app listening on port!', port);
+});
+
+
