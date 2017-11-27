b4dbd49ab6f2c46aad38531a2cdbedf714e8f903
pstuffa <p.buffa@gmail.com>
2017-11-27 14:15:30 -0500

    added projects

--- public/module/text-slide.js
+++ public/module/text-slide.js
@@ -3,30 +3,41 @@ function setTextSlide() {
 var margin = {top: 20, right: 20, bottom: 20, left: 20}
   , width = 760
   , height = 120
-  , firstWords = ["paul","small","tall","dr"]
-  , lastWords = ["buffa", "stuffa", "ruffa", "gruffa"];
+  , firstWords = ["paul","p"]
+  , lastWords = ["buffa", "stuffa"];
 
 function chart(selection) {
   selection.each(function(data) {
 
     var svg = d3.select(this);
 
+     var projects = svg.append("a")
+        .attr("xlink:href", "/projects")
+        .append("text")
+        .text("projects")
+        .attr("y", 100)
+        .attr("x", width-100)
+        .style("font-size", 20)
+        // .append("a")
+
     var firstWord = svg.append("text")
         .attr("class", "firstWord")
         .attr("y", 100)
         .attr("x", 100)
         .style("text-anchor", "end")
-        .text(firstWords[Math.floor(Math.random()*4)])
+        .text(firstWords[0])
 
     var lastWord = svg.append("text")
         .attr("class", "lastWord")
         .attr("y", 100)
         .attr("x", 102)
         .style("text-anchor", "start")
-        .text(lastWords[Math.floor(Math.random()*4)])
+        .text(lastWords[0])
 
+    var firstTimer = 0;
     d3.interval(function(elapsed) {
 
+      firstTimer = (firstTimer == 0 ? 1 : 0);
         firstWord.transition()
           .duration(1000)
           .attr("y", -100)
@@ -34,16 +45,19 @@ function chart(selection) {
           .transition()
           .duration(100)
           .attr("y", 100)
-          .text(firstWords[Math.floor(Math.random()*4)])
+          .text(firstWords[firstTimer])
           .transition()
           .duration(1000)
-          .style("opacity", 1);
+          .style("opacity", 1)
+          .style("fill", window.fontColor);
         
         }, 4000);
 
+    var secondTimer = 0;
      d3.timeout(function() {
        d3.interval(function(elapsed) {
 
+        secondTimer = (secondTimer == 0 ? 1 : 0);
           lastWord.transition()
             .duration(1000)
             .attr("y", height+100)
@@ -51,10 +65,11 @@ function chart(selection) {
             .transition()
             .duration(100)
             .attr("y", 100)
-            .text(lastWords[Math.floor(Math.random()*4)])
+            .text(lastWords[secondTimer])
             .transition()
             .duration(1000)
-            .style("opacity", 1);
+            .style("opacity", 1)
+            .style("fill", window.fontColor);
           
           }, 4000)
       }, 2000);
